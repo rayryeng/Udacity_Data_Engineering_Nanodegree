6cd42f075f010b5f95d334ddc79f75463a58820d
Ray Phan <ray@enlitic.com>
2020-12-29 21:36:08 -0500

    Fixed case for reserved keywords

--- project3-sparkify-aws-redshift/sql_queries.py
+++ project3-sparkify-aws-redshift/sql_queries.py
@@ -42,24 +42,24 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 # "userId":"101"}
 staging_events_table_create = ("""
 CREATE TABLE IF NOT EXISTS staging_events (
-    artist varchar,
-    auth varchar,
-    firstName varchar,
-    gender char(1),
-    itemInSession int,
-    lastName varchar,
-    length float,
-    level varchar,
-    location varchar,
-    method varchar,
-    page varchar,
-    registration float,
-    sessionId int,
-    song varchar,
-    status int,
-    ts bigint,
-    userAgent varchar,
-    userId int
+    artist VARCHAR,
+    auth VARCHAR,
+    firstName VARCHAR,
+    gender CHAR(1),
+    itemInSession INT,
+    lastName VARCHAR,
+    length FLOAT,
+    level VARCHAR,
+    location VARCHAR,
+    method VARCHAR,
+    page VARCHAR,
+    registration FLOAT,
+    sessionId INT,
+    song VARCHAR,
+    status INT,
+    ts BIGINT,
+    userAgent VARCHAR,
+    userId INT
 );
 """)
 
@@ -76,16 +76,16 @@ CREATE TABLE IF NOT EXISTS staging_events (
 #  "year": 0}
 staging_songs_table_create = ("""
 CREATE TABLE IF NOT EXISTS staging_songs (
-    num_songs int,
-    artist_id varchar,
-    artist_latitude float,
-    artist_longitude float,
-    artist_location varchar,
-    artist_name varchar,
-    song_id varchar,
-    title varchar,
-    duration float,
-    year int
+    num_songs INT,
+    artist_id VARCHAR,
+    artist_latitude FLOAT,
+    artist_longitude FLOAT,
+    artist_location VARCHAR,
+    artist_name VARCHAR,
+    song_id VARCHAR,
+    title VARCHAR,
+    duration FLOAT,
+    year INT
 );
 """)
 
@@ -93,56 +93,56 @@ CREATE TABLE IF NOT EXISTS staging_songs (
 # Also must prefix with int type
 songplay_table_create = ("""
 CREATE TABLE IF NOT EXISTS songplays (
-    songplay_id int IDENTITY(0,1) PRIMARY KEY sortkey, 
-    start_time timestamp NOT NULL, 
-    user_id int NOT NULL, 
-    level varchar, 
-    song_id varchar NOT NULL, 
-    artist_id varchar NOT NULL, 
-    session_id int NOT NULL, 
-    location varchar, 
-    user_agent varchar
+    songplay_id INT IDENTITY(0,1) PRIMARY KEY sortkey,
+    start_time timestamp NOT NULL,
+    user_id INT NOT NULL,
+    level VARCHAR,
+    song_id VARCHAR NOT NULL,
+    artist_id VARCHAR NOT NULL,
+    session_id INT NOT NULL,
+    location VARCHAR,
+    user_agent VARCHAR
 );
 """)
 
 user_table_create = ("""
 CREATE TABLE IF NOT EXISTS users (
-    user_id int PRIMARY KEY distkey, 
-    first_name varchar, 
-    last_name varchar, 
-    gender varchar, 
-    level varchar);
+    user_id int PRIMARY KEY distkey,
+    first_name VARCHAR,
+    last_name VARCHAR,
+    gender VARCHAR,
+    level VARCHAR);
 """)
 
 
 song_table_create = ("""
 CREATE TABLE IF NOT EXISTS songs (
-    song_id varchar PRIMARY KEY, 
-    title varchar, 
-    artist_id varchar distkey, 
-    year int, 
-    duration float
+    song_id VARCHAR PRIMARY KEY,
+    title VARCHAR,
+    artist_id VARCHAR distkey,
+    year INT,
+    duration FLOAT
 );
 """)
 
 artist_table_create = ("""
 CREATE TABLE IF NOT EXISTS artists (
-    artist_id varchar PRIMARY KEY distkey, 
-    name varchar, 
-    location varchar, 
-    latitude float, 
-    longitude float
+    artist_id VARCHAR PRIMARY KEY distkey,
+    name VARCHAR,
+    location VARCHAR,
+    latitude FLOAT,
+    longitude FLOAT
 );
 """)
 
 time_table_create = ("""
 CREATE TABLE IF NOT EXISTS time (
-    start_time timestamp PRIMARY KEY sortkey distkey, 
-    hour int, 
-    day int, 
-    week int, 
-    month int, 
-    year int, 
+    start_time timestamp PRIMARY KEY sortkey distkey,
+    hour INT,
+    day INT,
+    week INT,
+    month INT,
+    year INT,
     weekday int
 );
 """)
@@ -163,7 +163,7 @@ staging_songs_copy = ("""
     FROM {}
     CREDENTIALS 'aws_iam_role={}'
     COMPUPDATE OFF region 'us-west-2'
-    FORMAT AS JSON 'auto' 
+    FORMAT AS JSON 'auto'
     TRUNCATECOLUMNS BLANKSASNULL EMPTYASNULL;
 """).format(config.get("S3", "SONG_DATA"), config.get("IAM_ROLE", "ARN"))
 
@@ -194,7 +194,7 @@ SELECT DISTINCT userId AS user_id,
                 gender AS gender,
                 level AS level
 FROM staging_events
-where userId IS NOT NULL;
+WHERE userId IS NOT NULL;
 """)
 
 song_table_insert = ("""
@@ -216,7 +216,7 @@ SELECT DISTINCT artist_id AS artist_id,
                 artist_latitude AS latitude,
                 artist_longitude AS longitude
 FROM staging_songs
-where artist_id IS NOT NULL;
+WHERE artist_id IS NOT NULL;
 """)
 
 time_table_insert = ("""
